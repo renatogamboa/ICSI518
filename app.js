abc368c487ed63207f27861ddbe22e9ceb07dbd9
Renato Gamboa <38543901+renatogamboa@users.noreply.github.com>
2019-03-10 15:38:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'renato-gamboa',
+    bucket: '',
     acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
